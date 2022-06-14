b5467c8cbe58d54471f9e0fe2a50318821500fad
yoppytaro <yoppygood1201@docomo.ne.jp>
2022-06-14 21:25:01 +0900

    create category seeder

---
+++ present_graveyard_market/database/seeds/CategorySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::insert([
+            ['name' => 'レディース'],
+            ['name' => 'メンズ']
+        ]);
+
+    }
+}
